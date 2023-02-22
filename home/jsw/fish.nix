b073ef75dbe48b3a78c572a2b53f7cdc4f98b97d
Jurn Wubben <jurnwubben@gmail.com>
2023-02-22 20:37:54 +0100

    updated fish config

--- home/jsw/fish.nix
+++ home/jsw/fish.nix
@@ -2,5 +2,20 @@
 {
   programs.fish = {
     enable = true;
+    shellAliases = {
+      addup="git add -u";
+      add="git add .";
+      branch="git branch";
+      checkout="git checkout";
+      clone="git clone";
+      commit="git commit -m";
+      pull="git fetch";
+      push="git push origin";
+    };
   };	
+  programs.starship = {
+    enable = true;
+    enableFishIntergration = true;
+  };
+
 }
