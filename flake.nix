1b034d829dc62ee9e8bb698cf09bdd6c0e954ba9
Jurn Wubben <jurnwubben@gmail.com>
2023-02-23 18:34:46 +0100

    --interactive

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
     nixosConfigurations.jsw-pc = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = attrs;
+      overlays = [ hyprland ];
       modules = [ ./hosts/jsw-pc/default.nix ];
     };
     nixosConfigurations.jsw-server = nixpkgs.lib.nixosSystem {
