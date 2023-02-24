d586b7d9e8ae3bb2025754aed818f7c23e44b78b
Jurn Wubben <jurnwubben@gmail.com>
2023-02-24 19:06:08 +0100

    nvidia changes

--- hosts/jsw-pc/modules/nvidia.nix
+++ hosts/jsw-pc/modules/nvidia.nix
@@ -14,28 +14,28 @@ let
     exec "$@"
   '';
 in {
-  #boot.initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
-  #boot.extraModprobeConfig = "options nvidia-drm modeset=1";
-  #environment.sessionVariables = {
-  #  LIBVA_DRIVER_NAME = "nvidia";
-  #  XDG_SESSION_TYPE = "wayland";
-  #  #GBM_BACKEND="nvidia-drm";
-  #  __GLX_VENDOR_LIBARY_NAME = "nvidia";
-  #  WLR_NO_HARDWARE_CURSORS = "1";
-  #};
-
-  hardware.nvidia = {
-    prime = {
-      offload.enable = true;
-      intelBusId = "PCI:00:02:0";
-      nvidiaBusId = "PCI:01:00:0";
-    };
-    modesetting.enable = true;
-    powerManagement = {
-      enable = true;
-      finegrained = true;
-    };
+  boot.initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+  boot.extraModprobeConfig = "options nvidia-drm modeset=1";
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    XDG_SESSION_TYPE = "wayland";
+    #GBM_BACKEND="nvidia-drm";
+    __GLX_VENDOR_LIBARY_NAME = "nvidia";
+    WLR_NO_HARDWARE_CURSORS = "1";
   };
+
+  #hardware.nvidia = {
+  #  prime = {
+  #    offload.enable = true;
+  #    intelBusId = "PCI:00:02:0";
+  #    nvidiaBusId = "PCI:01:00:0";
+  #  };
+  #  modesetting.enable = true;
+  #  powerManagement = {
+  #    enable = true;
+  #    finegrained = true;
+  #  };
+  #};
   services.xserver.videoDrivers = [ "nvidia" ];
 
   environment.systemPackages = with pkgs; [
