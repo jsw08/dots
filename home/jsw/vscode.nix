e77baba6efb09f083d4c1effda6907321cc8fbaf
Jurn Wubben <jurnwubben@gmail.com>
2023-02-25 11:32:14 +0100

    added vscode extension + userSettings

--- home/jsw/vscode.nix
+++ home/jsw/vscode.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config; pkgs, ... }:
 {
   programs.vscode = {
     enable = true;
@@ -7,6 +7,14 @@
       vscode-extensions.vscodevim.vim
       vscode-extensions.ms-pyright.pyright
       vscode-extensions.github.copilot
+      vscode-extensions.bbenoist.nix
     ];
+    userSettings = {
+      "window.menuBarVisibility" = "hidden";
+      "vim.sneak" = true;
+      "vim.sneakReplacesF" = true;
+      "files.autoSave" = "onFocusChange";
+      "vim.useSystemClipboard" = true;
+    };
   };
 }
