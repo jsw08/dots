a76a87fec62b8c1850b1fac7be76757f8f1cf3ea
Jurn Wubben <jurnwubben@gmail.com>
2023-02-26 16:34:06 +0100

    -

--- modules/python.nix
+++ modules/python.nix
@@ -1,10 +1,8 @@
 { config, pkgs, ... }:
-{
-  let
-    my-python-packages = p: with p; [
-      pygame
-    ];
-  in
+let
+  my-python-packages = p: with p; [
+  pygame
+]; in {
   environment.systemPackages = [
     python3
     (pkgs.python3.withPackages my-python-packages)
