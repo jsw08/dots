485cfc4b817a07efc76b661e8b46b6ea37c5131d
Jurn Wubben <jurnwubben@gmail.com>
2023-02-26 16:34:22 +0100

    -

--- modules/python.nix
+++ modules/python.nix
@@ -3,7 +3,7 @@ let
   my-python-packages = p: with p; [
   pygame
 ]; in {
-  environment.systemPackages = [
+  environment.systemPackages = with pkgs; [
     python3
     (pkgs.python3.withPackages my-python-packages)
   ];
