1dedc47538bc1b370127415b161900ee32ca78d8
Jurn Wubben <jurnwubben@gmail.com>
2023-02-27 20:02:26 +0100

    change to waybar launch

--- home/jsw/hyprland/default.nix
+++ home/jsw/hyprland/default.nix
@@ -17,7 +17,7 @@
 
 			# Autostart
 			# exec-once=/usr/lib/polkit-kde-authentication-agent-1
-			exec = fish -c "pkill waybar; waybar"
+			exec-once = waybar
 			exec-once = swaybg -m fit -i /home/jsw/.config/hypr/background.jpg 
 
 			# Keyboard and mouse
