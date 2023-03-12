7db0486c427eb64fda4dd4b8597e119b31a7bce0
Jurn Wubben <jurnwubben@gmail.com>
2023-03-12 23:06:40 +0100

    moved polkit agent to security, changed alacritty colorscheme to tokyonight storm

--- home/jsw/alacritty.nix
+++ home/jsw/alacritty.nix
@@ -11,35 +11,40 @@
         size = 12;
       };
 
-      # Gruvbox
       colors = { 
-        primary = { 
-          background = "0x282828";
-          foreground = "0xebdbb2";
-        };
+        primary = {
+            background = "0x24283b";
+            foreground = "0xc0caf5";
+          };
 
         normal = {
-          black = "0x282828";
-          red = "0xcc241d";
-          green = "0x98971a";
-          yellow = "0xd79921";
-          blue = "0x458588";
-          magenta ="0xb16286";
-          cyan = "0x689d6a";
-          white = "0xa89984";
-        };
+            black =   "0x1d202f";
+            red =     "0xf7768e";
+            green =   "0x9ece6a";
+            yellow =  "0xe0af68";
+            blue =    "0x7aa2f7";
+            magenta = "0xbb9af7";
+            cyan =    "0x7dcfff";
+            white =   "0xa9b1d6";
+          };
 
-        bright = { 
-          black = "0x928374";
-          red = "0xfb4934";
-          green = "0xb8bb26";
-          yellow = "0xfabd2f";
-          blue = "0x83a598";
-          magenta = "0xd3869b";
-          cyan = "0x8ec07c";
-          white = "0xebdbb2";
-        };
+        bright = {
+            black =   "0x414868";
+            red =     "0xf7768e";
+            green =   "0x9ece6a";
+            yellow =  "0xe0af68";
+            blue =    "0x7aa2f7";
+            magenta = "0xbb9af7";
+            cyan =    "0x7dcfff";
+            white =   "0xc0caf5" ;
+          };
       };
+
+      indexed_colors = [
+        { index = 16; color = "0xff9e64"; }
+        { index = 17; color = "0xdb4b4b"; }
+      ];
+
     };
   };
 }
