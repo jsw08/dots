fa51190f5d39fdca4f6174723c139abb83196aa2
Jurn Wubben <jurnwubben@gmail.com>
2023-03-18 19:21:00 +0100

    finished update script

--- modules/core/n-update.nix
+++ modules/core/n-update.nix
@@ -2,21 +2,22 @@
 let update = pkgs.writeShellScriptBin "n-update" ''
   #!/usr/bin/env bash
   export TEMP=$(mktemp -d)
-  echo "Folder: $TEMP"
+  echo "# Folder: $TEMP"
 
   cd "$TEMP"
-  echo "Cloning repo..."
-  git clone https://github.com/jsw08/dots "$TEMP"
+  echo "# Cloning repo..."
+  nix flake clone "git+https://github.com/jsw08/dots.git" --dest $TEMP
 
-  echo "Updating flake..."
+  echo "# Updating flake..."
   nix flake update
   git add .
   git commit "flake update"
   echo "Pushing back to github..."
   git push
-  
+
   echo "Deleting folder..."
   rm -rf "$TEMP"
+  cd /
 
   echo "Updating system..."
   sudo nixos-rebuild boot --flake "git+https://github.com/jsw08/dots.git"
